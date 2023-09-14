fd010e5c1be9ba0b9deda487cbdcd72e803e4cd6
nebin <nebinbiju2002@gmail.com>
2023-09-14 13:13:07 +0530

    Initial commit

---
+++ lib/splashscreennew.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_splash_screen/flutter_splash_screen.dart';
+
+class SplashScreenPage extends StatelessWidget {
+  const SplashScreenPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterSplashScreen(
+
+    );
+  }
+}
